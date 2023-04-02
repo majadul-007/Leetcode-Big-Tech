95646b32000e3b8025a682aaaea55f3d7aa92747
Tusar Haque <tusarhaque12@gmail.com>
2023-04-02 09:52:23 +0600

    first commit

---
+++ 1.Array/1.Easy/1.Richest-Customer-Wealth.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+
+
+        //keep track of richest customer
+        int rich = 0;
+
+        //go through all the customer accounts
+        for (int[] customer : accounts) {
+
+            //keep track of sum
+            int sum = 0;
+
+            //go through each account and add it to sum
+            for (int accnt : customer) {
+
+                sum += accnt;
+            }
+
+            //compare previous customer rich or not
+
+            rich = Math.max(rich, sum);
+
+
+        }
+
+        return rich;
+
+
+
+    }
+}
