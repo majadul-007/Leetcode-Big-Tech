dccec88359470c2a5753b863e42ce7ba35cd6ae4
Tusar Haque <tusarhaque12@gmail.com>
2023-04-04 10:14:18 +0600

    first

--- 1.Array/1.Easy/3.Number-Of-GoodPairs.java
+++ 1.Array/1.Easy/3.Number-Of-GoodPairs.java
@@ -3,7 +3,10 @@ class Solution {
     public int numIdenticalPairs(int[] nums) {
 
         
+        //keep track of total good pairs
         int res = 0;
+
+        //loop through nums and check nums[i] equal or not with nums[j]
 
         for(int i=0; i<nums.length; i++){
 
