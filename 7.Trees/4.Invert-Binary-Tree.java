d368bb4d5fb49e8f7fda8f9fae832cbfb4ca11e1
Tusar Haque <tusarhaque12@gmail.com>
2023-05-28 11:42:28 +0600

    first

--- 7.Trees/4.Invert-Binary-Tree.java
+++ 7.Trees/4.Invert-Binary-Tree.java
@@ -1,8 +1,8 @@
 
-
-//Recursive approach
+//************************Recursive Approach********************
 
 class Solution {
+
     public TreeNode invertTree(TreeNode root) {
 
 
@@ -12,10 +12,12 @@ class Solution {
         if (root == null) {
 
             return null;
+
+
         }
 
-//store the left subtree in left sub tree
-//store the right subtree in right data type
+//store the left subtree in left node
+
 
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
@@ -26,6 +28,59 @@ class Solution {
         root.left = right;
         root.right = left;
 
+
+
+
+        return root;
+
+        
+
+
+//************************Iterative Approach*********************
+
+
+        if (root == null) {
+            return root;
+        }
+
+//initializing queue
+
+        Queue<TreeNode> myQ = new LinkedList();
+
+//add the root value
+
+        myQ.add(root);
+
+
+
+//iterate untill queue is empty
+
+        while (!myQ.isEmpty()) {
+
+//get first value from queue then swap the left and right child
+
+
+            TreeNode node = myQ.poll();
+
+            TreeNode temp = node.left;
+
+            node.left = node.right;
+            node.right = temp;
+
+
+//if node has any child left then add it to queue
+
+            if (node.left != null) {
+                myQ.add(node.left);
+            }
+            if (node.right != null) {
+                myQ.add(node.right);
+            }
+
+
+
+
+        }
 
         return root;
 
