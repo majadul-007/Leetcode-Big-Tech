52399f3e11f7fd111e3ac6f67952a317ef0f5160
majadul-007 <tusarhaque12@gmail.com>
2023-07-11 13:49:29 +0600

    first

---
+++ 9.Queue/2.Last-Stone-Weight.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+//maxheap for pushing the heaviest stone
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue();
+
+
+        for (int stone : stones) {
+            maxHeap.add(-stone);
+
+        }
+//while loop until maxheap has minimum one value
+
+        while (maxHeap.size() > 1) {
+
+            int stone1 = maxHeap.remove();
+            int stone2 = maxHeap.remove();
+
+//if stone1 not equal stone2 get the new weight by subtract
+
+            if (stone1 != stone2) {
+                int val = stone1 - stone2;
+                maxHeap.add(val);
+            }
+
+
+
+
+        }
+
+        return maxHeap.size() != 0 ? (-maxHeap.remove()) : 0;
+
+    }
+}
