92b3c965f2581d6dbb21b31f26ab3c3986bfbaa9
majadul-007 <tusarhaque12@gmail.com>
2023-12-17 13:08:20 +0600

    first

---
+++ 7.Trees/15.Validate-Binary-Search-Tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+        if(root == null){
+            return true;
+        }
+
+        return isBst(root, null, null);
+        
+    }
+
+    private boolean isBst(TreeNode root, Integer min, Integer max){
+
+        if(root == null){
+
+            return  true;
+        }
+
+        if(min != null && root.val <= min || max != null && root.val >= max){
+
+            return false;
+        }
+
+        return isBst(root.left, min, root.val) && isBst(root.right, root.val, max);
+
+    }
+}
